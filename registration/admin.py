1afe09c96265d0f373aa9803f80b4aba3e553276
tonythomas01 <01tonythomas@gmail.com>
2016-06-10 14:41:30 +0530

    Add user mark entry

--- registration/admin.py
+++ registration/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from registration.models import User,Student
+from registration.models import *
 
 class UserAdmin(admin.ModelAdmin):
     pass
@@ -7,6 +7,45 @@ search_fields = ('first_name', 'last_name', 'email')
 list_display = ('first_name', 'last_name', 'email')
 admin.site.register(User, UserAdmin)
 
+
+
 class StudentAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Student, StudentAdmin)
+
+
+
+class TestAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Test, TestAdmin)
+
+
+
+class TechTestAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(TechTest, TechTestAdmin)
+
+
+
+class QuantitativeTestAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(QuantitativeTest, QuantitativeTestAdmin)
+
+
+class ReasoningTestAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(ReasoningTest, ReasoningTestAdmin)
+
+
+class AptitudeTestAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(AptitudeTest, AptitudeTestAdmin)
+
+
+class EligibilityTestAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(EligibilityTest, EligibilityTestAdmin)
+
+class VerbalsTestAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(VerbalsTest, VerbalsTestAdmin)
